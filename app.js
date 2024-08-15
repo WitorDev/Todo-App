61fea7d509193159491a99c4352250c76350bedd
WitorDev <143912168+WitorDev@users.noreply.github.com>
2024-08-15 00:06:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let isPortuguese = false;
 
+let isPortuguese = false;
+
 function createTodo() {
     let textInput = document.getElementById('textInput');
     const todoContainer = document.getElementById('todoContainer');
@@ -10,6 +12,11 @@ function createTodo() {
     }
 
     if(textInput.value == ""){
+        if(isPortuguese){  
+            alert("Por favor nomeie ao afazer!");
+        } else {
+            alert("Please insert todo name!");
+        }
         if(isPortuguese){  
             alert("Por favor nomeie ao afazer!");
         } else {
@@ -47,11 +54,7 @@ function languageSwitch() {
     let languageSwitcher = document.getElementById("languageSwitcher");
 
     if(languageSwitcher.innerText != "Switch Language") {
-<<<<<<< HEAD
         title.innerText = "App de Afazeres";
-=======
-        title.innerText = "App de Afazeres"
->>>>>>> 6202ae2772462816215f0c01b5b6f3a10615f7e3
         inputLabel.innerHTML = 
         `Afazer: <input name="textInput" placeholder="Crie afazeres aqui..." maxlength="70" id="textInput" type="text">`;
         inputButton.innerText = "Criar Afazer";
@@ -59,20 +62,13 @@ function languageSwitch() {
 
         isPortuguese = true;
     } else {
-<<<<<<< HEAD
         title.innerText = "To do App";
-=======
-        title.innerText = "To do App"
->>>>>>> 6202ae2772462816215f0c01b5b6f3a10615f7e3
         inputLabel.innerHTML = 
         `Todo: <input name="textInput" placeholder="Set a todo here..." maxlength="70" id="textInput" type="text">`;
         inputButton.innerText = "Create Todo";
         languageSwitcher.innerText = "Trocar Idioma";
-<<<<<<< HEAD
         document.getElementById("credits").innerText = "Feito por: Witor Tenã"
 
-=======
->>>>>>> 6202ae2772462816215f0c01b5b6f3a10615f7e3
 
         isPortuguese = false;
     }
