6202ae2772462816215f0c01b5b6f3a10615f7e3
WitorDev <143912168+WitorDev@users.noreply.github.com>
2024-08-14 23:29:13 -0300

    Added Language switching and other features.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let isPortuguese = false;
+
 function createTodo() {
     let textInput = document.getElementById('textInput');
     const todoContainer = document.getElementById('todoContainer');
@@ -8,7 +10,11 @@ function createTodo() {
     }
 
     if(textInput.value == ""){
-        alert("Please insert todo name!");
+        if(isPortuguese){  
+            alert("Por favor nomeie ao afazer!");
+        } else {
+            alert("Please insert todo name!");
+        }
     } else {
         let currentTodo = todoContainer.childElementCount + 1;
         todoContainer.insertAdjacentHTML("beforeend", `<div class="todo-item-container"><input type="checkbox" onClick="doTodo(this.id)" id="todo-checkbox-${currentTodo}" class="todo-item-checkbox"><button onClick="deleteTodo(this.id)" id="todo-${currentTodo}" class="delete-todo-button"><i class="fa-solid fa-xmark"></i></button><p class="todo-item">${textInput.value}</p></div>`);
@@ -31,4 +37,33 @@ function doTodo(todoId) {
     let todoText = currentMarkedTodo.parentElement.lastChild;
 
     todoText.classList.toggle('marked-todo');
+}
+
+function languageSwitch() {
+
+    let title = document.getElementById("header-title");
+    let inputLabel = document.getElementById("input-label");
+    let inputButton = document.getElementById("createTodoBtn");
+    let languageSwitcher = document.getElementById("languageSwitcher");
+
+    if(languageSwitcher.innerText != "Switch Language") {
+        title.innerText = "App de Afazeres"
+        inputLabel.innerHTML = 
+        `Afazer: <input name="textInput" placeholder="Crie afazeres aqui..." maxlength="70" id="textInput" type="text">`;
+        inputButton.innerText = "Criar Afazer";
+        languageSwitcher.innerText = "Switch Language";
+
+        isPortuguese = true;
+    } else {
+        title.innerText = "To do App"
+        inputLabel.innerHTML = 
+        `Todo: <input name="textInput" placeholder="Set a todo here..." maxlength="70" id="textInput" type="text">`;
+        inputButton.innerText = "Create Todo";
+        languageSwitcher.innerText = "Trocar Idioma";
+
+        isPortuguese = false;
+    }
+
+
+
 }
